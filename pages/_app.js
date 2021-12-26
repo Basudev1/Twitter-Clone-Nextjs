80fa70a51d1029ea45db77cff5bea3b48f6a8022
Basudev <murabasudev@gmail.com>
2021-12-26 18:37:14 +0530

    Added Recoil, Moment, Modal Component

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,16 @@
 // pages/_app.js
 import '../styles/globals.css'
 import { SessionProvider } from "next-auth/react"
-
+import {RecoilRoot} from "recoil"
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
     <SessionProvider session={session}>
+      <RecoilRoot>
       <Component {...pageProps} />
+      </RecoilRoot>
     </SessionProvider>
   )
 }
